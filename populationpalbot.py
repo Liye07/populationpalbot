319bd616a9a097b5bb5302b65952f6cebe431cb5
Liyema Booi <s225025426@mandela.ac.za>
2024-08-13 14:20:03 +0200

    small changes

--- populationpalbot.py
+++ populationpalbot.py
@@ -4,24 +4,22 @@ import spacy
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
 
-# Load the spaCy model for natural language processing
 nlp = spacy.load("en_core_web_sm")
 
-# Define the states for the conversation
 ASK_COUNTRY, ASK_ANOTHER = range(2)
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text(
         "Hello there!\n\nMy name is PopulationPalBot🤖. \n\nI can help you find the population of any country. \n\nWhich country's population would you like to check?"
     )
-    return ASK_COUNTRY  # Move to the ASK_COUNTRY state
+    return ASK_COUNTRY 
 
 def get_population(country: str):
     response = requests.get(f"https://restcountries.com/v3.1/name/{country}")
     if response.ok and response.json():
         population = response.json()[0].get('population', 'Not available')
         return f"The population of {country.title()} is {population:,}."
-    return None  # Return None for invalid countries
+    return None
 
 def ask_country(update: Update, context: CallbackContext) -> int:
     user_message = update.message.text
@@ -34,27 +32,26 @@ def ask_country(update: Update, context: CallbackContext) -> int:
         update.message.reply_text("Sorry, I couldn't find that country. Please enter a valid country name.")
 
     update.message.reply_text("Would you like to find another country's population?")
-    return ASK_ANOTHER  # Move to the ASK_ANOTHER state
+    return ASK_ANOTHER
 
 def ask_another(update: Update, context: CallbackContext):
     user_response = update.message.text.strip().lower()
 
     if user_response in ['y', 'yes']:
         update.message.reply_text("Please enter the name of the country you'd like to check:")
-        return ASK_COUNTRY  # Go back to asking for a country name
+        return ASK_COUNTRY 
     elif user_response in ['n', 'no']:
         update.message.reply_text("Thank you for using PopulationPalBot🤖! \n\nDeuces✌🏽!")
-        return ConversationHandler.END  # End the conversation
+        return ConversationHandler.END 
     else:
         update.message.reply_text("Please respond with 'yes' or 'no'.")
-        return ASK_ANOTHER  # Stay in the ASK_ANOTHER state
+        return ASK_ANOTHER 
 
 def main():
     updater = Updater("7474298014:AAEHRMBvi1hyg5RAbpplx5In1zAzE8vpb2w", use_context=True)
 
     dp = updater.dispatcher
 
-    # Set up the ConversationHandler with states ASK_COUNTRY and ASK_ANOTHER
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
